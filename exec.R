0dc34d829b065b87e5d3cbe2e0869472f90ac417
abblake <72878138+abblake@users.noreply.github.com>
2021-05-30 17:58:40 -0500

    Update exec.R

--- exec.R
+++ exec.R
@@ -1,5 +1,7 @@
 #exec pull
 u_gvkey <- unique(df$gvkey)
-first_pull_exec <- tbl(wrds, sql("select * from comp_execucomp.anncomp where year between '",year_start,"'"," and '", year_end,"'"))
+first_pull_exec <- tbl(wrds, sql("select * from comp_execucomp.anncomp"))
+first_pull_exec <- first_pull_exec %>% filter(between(year, year_start,year_end))
 first_pull_exec <- first_pull_exec %>% filter(gvkey %in% u_gvkey)
 pull_exec <- first_pull_exec %>% collect()
+
