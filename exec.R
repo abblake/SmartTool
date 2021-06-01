8056f99bc6feb141ce15a233d8a410923be345c0
abblake <72878138+abblake@users.noreply.github.com>
2021-06-01 11:38:52 -0500

    Update exec.R

--- exec.R
+++ exec.R
@@ -4,4 +4,5 @@ first_pull_exec <- tbl(wrds, sql("select * from comp_execucomp.anncomp"))
 first_pull_exec <- first_pull_exec %>% filter(between(year, year_start,year_end))
 first_pull_exec <- first_pull_exec %>% filter(gvkey %in% u_gvkey)
 exec <- first_pull_exec %>% collect()
-cols_to_remove <- c(names(df),names(exec)) %>% distinct() %>% select(-gvkey,-year,-tic,-sich)
+cols_to_remove <- c(names(df),names(exec)) %>% distinct()
+cols_to_remove <- cols_to_remove[cols_to_remove %notin% c('gvkey','fyear','year','tic','sich','cusip','cik','execid')]
