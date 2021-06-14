55a3cd70701e940e343ec2ed69470ddd6c8ea3ba
abblake <72878138+abblake@users.noreply.github.com>
2021-06-14 09:04:07 -0500

    Update exec.R

--- exec.R
+++ exec.R
@@ -1,8 +1,14 @@
 #exec pull
+#create object with unique gkvey
 u_gvkey <- unique(df$gvkey)
+#lazy pull execucomp data
 first_pull_exec <- tbl(wrds, sql("select * from comp_execucomp.anncomp"))
+#filter by input years
 first_pull_exec <- first_pull_exec %>% filter(between(year, year_start,year_end))
+#filter by gvkey object
 first_pull_exec <- first_pull_exec %>% filter(gvkey %in% u_gvkey)
+#full data pull
 exec <- first_pull_exec %>% collect()
+#for clean up later
 cols_to_remove <- c(names(df),names(exec))
 cols_to_remove <- cols_to_remove[cols_to_remove %notin% c('gvkey','fyear','year','tic','sich','cusip','cik','execid')]
