a9372e74907eb7fefe0ee17094765131ab852a80
abblake <72878138+abblake@users.noreply.github.com>
2021-06-14 09:02:32 -0500

    Update firm_age.R

--- firm_age.R
+++ firm_age.R
@@ -1,8 +1,13 @@
 ###firm_age
+#extract ipodate
 ipodate <- tbl(wrds, sql("select gvkey, ipodate from comp_na_daily_all.company"))
+#create object with only unique gvkeys
 f_gvkey <- unique(df$gvkey)
+#filter by object
 ipodate <- ipodate %>% filter(gvkey %in% f_gvkey)
+#collect data from wrds
 ipo_df <- ipodate %>% collect()
+#merge
 df <- merge(df, ipo_df, by='gvkey', all.x = T)
 df <- df %>% mutate(ipo_year = year(ipodate))
 df <- df %>% mutate(firm_age = year - ipo_year)
