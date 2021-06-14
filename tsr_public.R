3c91724670fcc5de1fec314f877590fbe07e2b28
abblake <72878138+abblake@users.noreply.github.com>
2021-06-14 08:59:33 -0500

    Update tsr_public.R

--- tsr_public.R
+++ tsr_public.R
@@ -7,7 +7,7 @@ pacman::p_load(dplyr, RPostgres, lubridate, haven, stringr, svDialogs, janitor, 
 
 #let's do some prep work to make things easy on us after the data is pulled
 
-#first off, we need to link gvkeys to permno. Fortunately, we can source a script from github.
+#first off, we need to link gvkeys to permno.
 comp <- tbl(wrds, sql("select a.*, b.* from security as a, company as b
                       where a.gvkey = b.gvkey"))
 comp <- comp %>% filter(!is.na(isin))
@@ -34,22 +34,29 @@ crsp <- tbl(wrds, sql('select *, extract(year from date) as year, lag(ret,1) ove
                       from crsp.msf'))
 
 
-
+#we create function here to add 1 to all RET values.
 add_one <- function(x) {x+1}
+#create sequence of the years you specified. this is important for filtering data
 year_seq <- seq(year_start, year_end,1)
+#filter by year sequence
 crsp <- crsp %>% filter(year %in% year_seq)
+#filter by cusip
 crsp <- crsp %>% filter(cusip %in% u_cusip)
+#collect data from crsp
 crsp <- crsp %>% collect(); print('This part usually takes about 5 minutes, please be patient. Do not worry, the heavy processing is being done by WRDS server.')
 crsp <- crsp %>% distinct(permno, permco, date, ret, .keep_all = T) #drop duplicates
 crsp <- crsp %>% mutate_at(vars(matches("ret")), add_one) #add one to TSR
 
 
 #paste0('ret','_',"l",seq(1:11), collapse = "*") get formula
+#calculate tsr
 tsr_calc <- crsp %>% group_by(permno, year) %>% filter(date==max(date)) %>% mutate(tsr_annual = ((ret*ret_l1*ret_l2*ret_l3*ret_l4*ret_l5*ret_l6*ret_l7*ret_l8*ret_l9*ret_l10*ret_l11)-1)*100)
+#isolate specific variables.
 tsr_calc <- tsr_calc %>% select(cusip, permno, permco, date, year, tsr)
 
 rm(crsp)
 
+#here we are merging linking information so that TSR can be merged to our larger dataframe.
 comp <- comp %>% select(gvkey,cusip,cusip_8)
 tsr_calc <- merge(tsr_calc, comp, by.x = 'cusip', by.y = 'cusip_8', all.x = T)
 tsr_calc <- tsr_calc %>% rename(cusip_9 = cusip.y) %>% rename(cusip_8 = cusip)
