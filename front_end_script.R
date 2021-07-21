1c9213750aac3792b0fcae5acdee23f271f90937
abblake <72878138+abblake@users.noreply.github.com>
2021-07-21 13:04:26 -0500

    Update front_end_script.R

--- front_end_script.R
+++ front_end_script.R
@@ -18,6 +18,11 @@ div_code <- FALSE
           year_start <- dlg_input(message = "What year should your sample start? (e.g., 2000)")$res %>% as.numeric()
             year_end <- dlg_input(message = "What year should your sample end? (e.g., 2000)")$res %>% as.numeric()
 
+		#enter exchanges you would like to pull from
+		exchg_list <- dlg_input(message = "Enter Stock Exchange Codes -- default is NYSE and US NASDAQ", default = "11,14,15",  gui = .GUI)$res
+		exchg_list <- regmatches(exchg_list, gregexpr("[[:digit:]]+", exchg_list)) 
+		exchg_list <- as.numeric(unlist(exchg_list))
+  
 print('*******Database pull has begun. This will take some time (5-10 minutes), please be patient.*******')
               ###adapted from https://wrds-www.wharton.upenn.edu/, connects you to WRDS DB
             print("Please enter your WRDS credentials.")
@@ -47,20 +52,12 @@ if(!exists('wrds')){
               }
 
             ###connect and pull compustat data based on year
-                pulled <- dbSendQuery(wrds, paste0("select *
-                   from comp.funda
-                   where fyear between '",year_start,"'","
-                   and '",year_end,"'
-                   and datafmt = 'STD'
-                   and consol = 'C'
-                   and indfmt = 'INDL'
-                   and popsrc = 'D'
-		   and exchg = '11' 
-		   or exchg = '14' 
-		   or exchg = '15'  "))
-                  df <- dbFetch(pulled, n=-1)
-                    dbClearResult(pulled)
-                    df$year <- df$fyear
+          	pulled <- tbl(wrds, sql("select * from comp.funda where datafmt = 'STD' and consol = 'C' and indfmt = 'INDL' and popsrc = 'D'"))
+		pulled <- pulled %>% filter(between(fyear, year_start,year_end))
+		pulled <- pulled %>% filter(exchg %in% exchg_list)
+		df <- pulled %>% collect()
+		rm(pulled)
+                df$year <- df$fyear
 #get vector of unique gvkeys
 u_gvkey <- unique(df$gvkey)
 #this will be used to create the 'small' dataframe at the end of the script
