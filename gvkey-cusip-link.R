5741564277e88fe6de1be93cae1a5433263f7bcd
abblake <72878138+abblake@users.noreply.github.com>
2021-07-24 14:23:54 -0500

    Update gvkey-cusip-link.R

--- gvkey-cusip-link.R
+++ gvkey-cusip-link.R
@@ -1,7 +1,7 @@
 comp_gvkeyfix <- tbl(wrds, sql("select a.*, b.* , a.gvkey as gvkey_link from security as a, company as b
                       where a.gvkey = b.gvkey"))
 comp_gvkeyfix <- comp_gvkeyfix %>% filter(!is.na(isin))
-comp_gvkeyfix <- comp_gvkeyfix %>% filter(exchg==11)
+#comp_gvkeyfix <- comp_gvkeyfix %>% filter(exchg==11)
 comp_gvkeyfix <- comp_gvkeyfix %>% select(gvkey_link, cusip) %>% distinct()
 comp_gvkeyfix <- comp_gvkeyfix %>% mutate(cusip6 = substr(cusip,1,6))
 comp_gvkeyfix <- comp_gvkeyfix %>% collect()
