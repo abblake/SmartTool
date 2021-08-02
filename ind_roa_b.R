ab0b8d104e35167d689eec0ab99549b2095c4b3d
abblake <72878138+abblake@users.noreply.github.com>
2021-08-02 14:32:24 -0500

    Update ind_roa_b.R

--- ind_roa_b.R
+++ ind_roa_b.R
@@ -34,7 +34,7 @@ industry_pull <- industry_pull %>% mutate(sic_2 = substr(sich,1,2))
 industry_pull <- industry_pull %>% filter(sic_2 %in% sic2)
 
 #create lag variable for at
-industry_pull <- industry_pull %>% group_by(gvkey) %>% mutate(at_l = dplyr::lag(at),1,order_by = fyear) %>% ungroup()
+industry_pull <- industry_pull %>% group_by(gvkey) %>% mutate(at_l = dplyr::lag(at),1,order_by = year) %>% ungroup()
 #compute ROA
 industry_pull <- industry_pull %>% group_by(gvkey, fyear) %>% mutate(roa_temp = ni/(((at+at_l)/2))) %>% ungroup()
 industry_pull <- industry_pull %>% group_by(sic_2, year) %>% mutate(ind_roa2 = mean(roa_temp, na.rm=T)) %>% ungroup()
