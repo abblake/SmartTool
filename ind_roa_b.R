1894b66a744a75a7923309a5f2e994e10d101543
abblake <72878138+abblake@users.noreply.github.com>
2021-08-02 14:04:22 -0500

    Update ind_roa_b.R

--- ind_roa_b.R
+++ ind_roa_b.R
@@ -9,7 +9,7 @@ df$sich <- stringr::str_pad(df$sich, 4, 'left', '0')
       sic2 <- sic2[!is.na(sic2)]
 
 #initial lazy pull
-industry_pull <- tbl(wrds, sql("select lpad(sich::text, 4,'0') as SICH, fyear as YEAR, at, ni
+industry_pull <- tbl(wrds, sql("select lpad(sich::text, 4,'0') as SICH, fyear as YEAR, at, ni, gvkey
 from compa.funda"))
 
 #filter year based on what we want
