258974b645d682b1556e4466131178c262e2e169
abblake <72878138+abblake@users.noreply.github.com>
2021-08-02 14:13:55 -0500

    Update ind_roa_a.R

--- ind_roa_a.R
+++ ind_roa_a.R
@@ -34,7 +34,7 @@ industry_pull <- industry_pull %>% mutate(sic_2 = substr(sich,1,2))
 industry_pull <- industry_pull %>% filter(sic_2 %in% sic2)
 #compute average sic2 roa per year
 industry_pull <- industry_pull %>% group_by(gvkey) %>% arrange(year, .by_group = T) %>% mutate(roa_temp = ni/at) %>% ungroup()
-industry_pull <- industry_pull %>% group_by(sic_2, year) %>% summarize(ind_roa1 = mean(roa_temp, na.rm=T)) %>% ungroup()
+industry_pull <- industry_pull %>% group_by(sic_2, year) %>% summarise(ind_roa1 = mean(roa_temp, na.rm=T)) %>% ungroup()
 #drop duplicates
 industry_pull <- industry_pull %>% distinct(year,sic_2, ind_roa1, .keep_all = F)
 df <- merge(df,industry_pull, by=c('sic_2', 'year'), all.x=T)
