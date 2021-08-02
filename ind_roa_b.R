02b3384090291608936ffed994da907761eb9368
abblake <72878138+abblake@users.noreply.github.com>
2021-08-02 13:11:53 -0500

    Update ind_roa_b.R
    SICH fix and other adjustments.

--- ind_roa_b.R
+++ ind_roa_b.R
@@ -8,32 +8,36 @@ df$sich <- stringr::str_pad(df$sich, 4, 'left', '0')
       #drop NA
       sic2 <- sic2[!is.na(sic2)]
 
-        #initial lazy pull
-        industry_pull <- tbl(wrds, sql("select lpad(sich::text, 4,'0') as SICH, fyear as YEAR, at, ni
+#initial lazy pull
+industry_pull <- tbl(wrds, sql("select lpad(sich::text, 4,'0') as SICH, fyear as YEAR, at, ni
 from compa.funda"))
-          #drop NA sic codes
-          industry_pull <- industry_pull %>% filter(!is.na(sich))
-            #create sic 2 codes
-            industry_pull <- industry_pull %>% mutate(sic_2 = substr(sich,1,2))
-              #filter sic 2 codes based on what we have in original pull
-              industry_pull <- industry_pull %>% filter(sic_2 %in% sic2)
-                #filter year based on what we want
-                industry_pull <- industry_pull %>% filter(between(year, year_start,year_end))
-                  #drop observations that equal 0 for assets
-                  industry_pull <- industry_pull %>% filter(at != 0)
-                    #drop observations that equal 0 for net income
-                    industry_pull <- industry_pull %>% filter(ni != 0)
-                      #actual data pull
-                        industry_roa_b <- industry_pull %>% collect()
-                      #create lag variable for at
-                        industry_roa_b <- industry_roa_b %>% arrange(sich, year) %>% mutate(at_l = dplyr::lag(at, 1))
-                      #compute average sic2 roa per year
-                        industry_roa_b <- industry_roa_b %>% group_by(sic_2, year) %>% mutate(ind_roa2 = mean(ni/(((at+at_l)/2)), na.rm = T)) %>% ungroup()
-                        #drop duplicates
-                        industry_roa_b <- industry_roa_b %>% distinct(year,sic_2, ind_roa2, .keep_all = F)
 
+#filter year based on what we want
+industry_pull <- industry_pull %>% filter(between(year, year_start,year_end))
 
+#replace missing sich with SIC codes
+industry_pull <- industry_pull %>% collect()
+missing_sich <- unique(industry_pull$gvkey)
+sich_pull <- tbl(wrds, sql('select * from company'))
+sich_pull <- sich_pull %>% filter(gvkey %in% missing_sich)
+sich_pull <- sich_pull %>% collect()
+sich_pull <- sich_pull %>% select(gvkey, sic)
+industry_pull <- merge(industry_pull, sich_pull, by='gvkey', all.x = T)
+industry_pull$sich <- ifelse(is.na(industry_pull$sich), industry_pull$sic, industry_pull$sich)
+industry_pull <- industry_pull %>% select(-sic)
+rm(sich_pull, missing_sich)
+#drop NA sic codes
+industry_pull <- industry_pull %>% filter(!is.na(sich))
+#create sic 2 codes
+industry_pull <- industry_pull %>% mutate(sic_2 = substr(sich,1,2))
+#filter sic 2 codes based on what we have in original pull
+industry_pull <- industry_pull %>% filter(sic_2 %in% sic2)
 
-
-                        df <- merge(df,industry_roa_b, by=c('sic_2', 'year'), all.x=T)
-                        rm(industry_roa_b)
+#create lag variable for at
+industry_pull <- industry_pull %>% group_by(gvkey) %>% mutate(at_l = dplyr::lag(at),1,order_by = fyear)
+#compute average sic2 roa per year
+industry_pull <- industry_pull %>% group_by(sic_2, year) %>% mutate(ind_roa2 = mean( ni/ ( (at+at_l) /2 ) , na.rm = T)) %>% ungroup()
+#drop duplicates
+industry_pull <- industry_pull %>% distinct(year,sic_2, ind_roa2, .keep_all = F)
+df <- merge(df,industry_roa_b, by=c('sic_2', 'year'), all.x=T)
+rm(industry_pull)
