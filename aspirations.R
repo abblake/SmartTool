195fbe8f5c2db88920e86fbc4756833e4a2f1498
abblake <72878138+abblake@users.noreply.github.com>
2021-08-02 13:37:40 -0500

    Update aspirations.R
    SICH fix and mean computation adjustment

--- aspirations.R
+++ aspirations.R
@@ -17,30 +17,41 @@ sic2 <- sic2[!is.na(sic2)]
 #initial lazy pull
 industry_pull <- tbl(wrds, sql("select lpad(sich::text, 4,'0') as SICH, fyear as YEAR, at, ni
                    from compa.funda"))
+#filter year based on what we want
+industry_pull <- industry_pull %>% filter(between(year, year_start,year_end))
+
+#replace missing sich with SIC codes
+industry_pull <- industry_pull %>% collect()
+missing_sich <- unique(industry_pull$gvkey)
+sich_pull <- tbl(wrds, sql('select * from company'))
+sich_pull <- sich_pull %>% filter(gvkey %in% missing_sich)
+sich_pull <- sich_pull %>% collect()
+sich_pull <- sich_pull %>% select(gvkey, sic)
+industry_pull <- merge(industry_pull, sich_pull, by='gvkey', all.x = T)
+industry_pull$sich <- ifelse(is.na(industry_pull$sich), industry_pull$sic, industry_pull$sich)
+industry_pull <- industry_pull %>% select(-sic)
+rm(sich_pull, missing_sich)
+
+
 #drop NA sic codes
 industry_pull <- industry_pull %>% filter(!is.na(sich))
 #create sic 2 codes
 industry_pull <- industry_pull %>% mutate(sic_2 = substr(sich,1,2))
 #filter sic 2 codes based on what we have in original pull
 industry_pull <- industry_pull %>% filter(sic_2 %in% sic2)
-#filter year based on what we want
-industry_pull <- industry_pull %>% filter(between(year, year_start,year_end))
-#drop observations that equal 0 for assets
-industry_pull <- industry_pull %>% filter(at != 0)
-#drop observations that equal 0 for net income
-industry_pull <- industry_pull %>% filter(ni != 0)
+
 #compute average sic2 roa per year
-industry_pull <- industry_pull %>% group_by(sic_2, year) %>% mutate(social_a = mean(ni/at, na.rm = T)) %>% ungroup()
+industry_pull <- industry_pull %>% group_by(gvkey) %>% arrange(year, .by_group = T) %>% mutate(roa_temp = ni/at) %>% ungroup()
+industry_pull <- industry_pull %>% group_by(sic_2, year) %>% summarize(social_a = mean(roa_temp, na.rm=T) %>% ungroup()
 #drop duplicates
-industry_pull <- industry_pull %>% distinct(year,sic_2, social_a, .keep_all = F)
+industry_pull <- industry_pull %>% distinct(sic_2, year, .keep_all = F)
 
 #actual data pull
-social_asip <- industry_pull %>% collect()
 df$year <- df$fyear
-df <- merge(df,social_asip, by=c('sic_2', 'year'), all.x=T)
+df <- merge(df,industry_pull, by=c('sic_2', 'year'), all.x=T)
+rm(industry_pull)
 if(length(df$total_assets) > 0){
 df$at <- df$total_assets
   }
 df <- df %>% group_by(gvkey) %>% mutate(temp_roa = ni/at) %>% mutate(historical_a = dplyr::lag(temp_roa,1,order_by=fyear)) %>% select(-temp_roa)
 
-rm(social_asip)
