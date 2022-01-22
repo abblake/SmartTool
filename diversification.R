44f5308edd087986261e4fd5e59cf4df07e2013a
abblake <72878138+abblake@users.noreply.github.com>
2022-01-21 18:21:09 -0600

    Update diversification.R
    Updating script to annual instead of quarterly

--- diversification.R
+++ diversification.R
@@ -3,39 +3,29 @@
 if (!require('pacman')) install.packages('pacman')
 pacman::p_load(dplyr, RPostgres, lubridate, haven, stringr, svDialogs, janitor)
 
-#converting years for compustat
-date_start <- paste0(year_start,'-01-01')
-date_end <- paste0(year_end,'-12-31')
-
-#connecting to historical segments
-segment <- tbl(wrds, sql(paste0("select *, extract(year from datadate) AS year from comp_segments_hist_daily.wrds_segmerged
-                   where stype = 'BUSSEG'
-                   and datadate between '",date_start,"'
-                   and '",date_end,"'")))
-#remove NA sectors
-segment <- segment %>% filter(!is.na(sics1))
-#remove NA sales variable
-segment <- segment %>% filter(!is.na(sales))
-#collect information
-div <- segment %>% collect()
-#add in cols to remove
-cols_to_remove <- c(cols_to_remove, names(div))
-cols_to_remove <- cols_to_remove[cols_to_remove %notin% c('gvkey','fyear','year','tic','sich','cusip','cik','execid')]
-#suming all sales by sector and year
-div <- div %>% group_by(sics1, year) %>% mutate(sector_sales = sum(sales, na.rm = T))
-#remove sectors with zero sales
-div <- div %>% filter(sector_sales != 0)
-#calulating p ratio
-div <- div %>% group_by(gvkey,year) %>% mutate(p_ratio = sales / sector_sales)
-#calculate entropy according to #CEO POLITICAL IDEOLOGIES AND PAYEGALITARIANISM WITHIN TOP MANAGEMENTTEAMS
-div <- div %>% group_by(gvkey,year) %>% mutate(pre_calculation = p_ratio * log(p_ratio)) %>% mutate(entropy = sum(pre_calculation, na.rm = T))
-
-#get final numbers for year
-div <- div %>% distinct(gvkey,year, .keep_all = T) %>% select(gvkey,year,datadate, entropy)
+#pulling historical segments
+  lazy_pull <- tbl(wrds, sql("select * from comp.wrds_segmerged"))
+  lazy_pull <- lazy_pull %>% filter(gvkey %in% u_gvkey)
+  lazy_pull <- lazy_pull %>% filter(stype == "BUSSEG")
+#downloading data
+div <- lazy_pull %>% collect()
+#create year variable and drop duplicates to source date clostest to fiscal year
+  div$datadate <- ymd(div$datadate)
+  div$srcdate <- ymd(div$srcdate)
+  div$year <- year(div$datadate)
+  div <- div %>% filter(between(year, year_start,year_end))
+  #drop duplicates by the date that is closests to fiscal year
+  div <- div %>% group_by(stype, gvkey, datadate) %>% filter(srcdate==min(srcdate))
+#compute diversification
+  div <- div %>% group_by(stype, gvkey, datadate) %>% mutate(total_sales = sum(sales, na.rm = T))
+  div <- div %>% group_by(stype, gvkey, datadate, sid) %>% mutate(p_ia = sales/total_sales)
+  div <- div %>% group_by(stype, gvkey, datadate, sid) %>% mutate(p_ia_formula = p_ia*log(1/p_ia))
+  div <- div %>% group_by(stype, gvkey, datadate) %>% mutate(diversification1 = sum(p_ia_formula, na.rm = T)) %>% ungroup()
+  div <- div %>% select(gvkey, datadate, year, diversification1)
+  div <- div %>% distinct(gvkey, datadate, .keep_all = T)
 
 
 ####follow these steps to merge this data with CSV datafile computed with START
-df$year <- df$fyear
 df <- merge(df, div, by=c('gvkey', 'year'), all.x=T)
 rm(div)
 
